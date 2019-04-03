288117c3c615321d9a10d30cc8d3a173b579f6e2
cubanerick <37166192+cubanerick@users.noreply.github.com>
2019-04-03 11:50:03 -0700

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,13 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO 
+      title="Erick Echeverria | 404 Page" 
+      description='I am a full-stack web developer experienced working with interdisciplinary teams to build high quality websites, that follow best practices, SEO, and responsive design. I have developed, launched, and maintained multiple projects, including setting up Dev-Ops and hosting. I have a passion for writing clean code and continued education in the computer sciences.' 
+      author='Erick Echeverria' 
+      keywords={[`Portfolio`, `Erick Echeverria`, 'Full Stack Developer, JS Engineer']} 
+      lang='EN' 
+    />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
