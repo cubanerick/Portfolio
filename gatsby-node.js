eec9882c2e14e5e583a151bbbdc7ba5a6bf92153
cubanerick <37166192+cubanerick@users.noreply.github.com>
2019-04-03 15:42:36 -0700

    netlify

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
+createRedirect({ fromPath: "http://erickecheverria.com", toPath: "https://erickecheverria.tech", isPermanent: true })
+createRedirect({ fromPath: "https://erickecheverria.com", toPath: "https://erickecheverria.tech", isPermanent: true })
+createRedirect({ fromPath: "https://www.erickecheverria.com", toPath: "https://erickecheverria.tech", isPermanent: true })
 // You can delete this file if you're not using it
